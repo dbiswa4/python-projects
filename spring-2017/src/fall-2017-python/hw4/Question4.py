8183c71311e04fd2302fd781b690d5252e1c3162
Dipankar Biswas <dipankar.biswas@teamaol.com>
2017-09-24 12:06:45 -0700

    hw4 file read issue fixed

--- spring-2017/src/fall-2017-python/hw4/Question4.py
+++ spring-2017/src/fall-2017-python/hw4/Question4.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
+
 #Question 4
 def squareEach(nums):
     temp_list = []
     for i in nums:
-        temp_list.append(i*i)
+        temp_list.append(float(i)*float(i))
 
 
 def sumList(nums):
-    temp = 0
+    temp = 0.0
     for i in nums:
-        temp +=i
+        temp += float(i)
     return temp
 
 
@@ -16,20 +18,20 @@ def sumList(nums):
 def toNumbers(strList):
     temp_list=[]
     for i in strList:
-        temp_list.append(int(i))
+        temp_list.append(float(i))
 
 
 
 def main():
     print("The program prints the sum of squares of each line in a file.")
     fname = input("Enter the name of file with numbers: ")
+    #fname = raw_input("Enter the name of file with numbers: ")
     count = 1
     f = open(fname, 'r')
     for line in iter(f):
         this_line = line.rstrip('\n')
         if len(this_line) != 0:
-            temp = []
-            temp.append(this_line.split())
+            temp = this_line.split()
             print(temp)
             toNumbers(temp)
             squareEach(temp)
